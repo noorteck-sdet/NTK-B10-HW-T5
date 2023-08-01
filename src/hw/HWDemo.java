f14b170581ec1883ff2eb89c22151d3b21b3954a
AnnelisseM <AnnelisseM@CIA>
2023-08-01 08:07:21 -0700

    Added ArrayList teamLectures and rearranged order of methods

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -26,21 +26,44 @@ public class HWDemo {
 
 		stateMap.put("FL", "Florida");
 		System.out.println(stateMap);
-		
+
 		HashMap<String, Double> priceMap = new HashMap<String, Double>();
 		priceMap.put("Iphone", 999.99);
 		priceMap.put("MacBook", 1999.99);
 		System.out.println(priceMap.get("Iphone"));
+
+		ArrayList<String> teamLectures = new ArrayList<String>();
+
+		teamLectures.add("Software Testing");
+		teamLectures.add("Java");
+		teamLectures.add("Git");
+		teamLectures.add("SQL");
+		teamLectures.add("Functional Testing");
+		teamLectures.add("Maven & Jenkins");
+		teamLectures.add("Selenium");
+		teamLectures.add("Cucumber");
+		teamLectures.add("Framework");
+		teamLectures.add("JUnit & Testing");
+		teamLectures.add("API");
+
+		System.out.println("Team5: " + teamLectures);
+
 	}
-	
+
+	public static void fun(String a) {
+		System.out.println("Java is fun!");
+	}
+
+	public static void getWeather() {
+		System.out.println("Today is sunny");
+	}
+
 	public static void numFav(boolean a) {
 		System.out.println("true");
-
 	}
 
 	public static void car() {
 		System.out.println("Toyota");
-
 	}
 
 	public static void getDate() {
@@ -51,18 +74,11 @@ public class HWDemo {
 		System.out.println("Info");
 	}
 
-	public static void fun(String a) {
-		System.out.println("Java is fun!");
-	}
-
-	public static void getWeather() {
-		System.out.println("Today is sunny");
-	} 
-	
 	public static void getColor() {
 		System.out.println("RED");
-	} 
-	public static void favPhone () {
+	}
+
+	public static void favPhone() {
 		System.out.println("iphone");
 	}
 }
